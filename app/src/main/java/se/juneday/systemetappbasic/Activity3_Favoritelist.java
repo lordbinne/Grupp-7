071b0440ce7e852a1ac2a941ac1efc3e151b4937
agojkovic <gusgojal@student.gu.se>
2019-05-17 11:11:43 +0100

    Lagt till favorit-knapp + text

---
+++ app/src/main/java/se/juneday/systemetappbasic/Activity3_Favoritelist.java
@@ -0,0 +1,16 @@
+package se.juneday.systemetappbasic;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Activity3_Favoritelist extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity3_favoritelist);
+    }
+}
